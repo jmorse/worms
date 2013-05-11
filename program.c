af03955c6882d1afff7bfb370ce299688b6b0381
Jeremy Morse <jmorse+git@studentrobotics.org>
2013-05-11 22:58:32 +0100

    Opencl objects to stdint.h it seems.

--- program.c
+++ program.c
@@ -1,16 +1,14 @@
-#include <stdint.h>
-
-void group_leader(__global uint8_t *match_configs)
+void group_leader(__global char *match_configs)
 {
 	return;
 }
 
-void group_worker(__global uint8_t *match_configs)
+void group_worker(__global char *match_configs)
 {
 	return;
 }
 
-__kernel void start_trampoline(__global uint8_t *match_configs)
+__kernel void start_trampoline(__global char *match_configs)
 {
 	if (get_local_id(0) == 0)
 		group_leader(match_configs);
