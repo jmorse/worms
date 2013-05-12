34f818ebe0539bf77576f7b45d28a70160271754
Jeremy Morse <jmorse+git@studentrobotics.org>
2013-05-12 15:47:25 +0100

    Load match configs into each local proc.

--- program.c
+++ program.c
@@ -5,8 +5,15 @@
 __kernel void start_trampoline(__global char *match_configs,
 				__global char *output)
 {
+	__private unsigned int i, startloc;
+	// Per worker match configs.
+	__private char local_match_configs[CONFIGS_PER_PROC * sizeof(char) * 4];
 
-	__private unsigned int i;
+	// Read in per worker match configs
+	startloc = get_local_id(0) * CONFIGS_PER_PROC * 4;
+	for (i = 0; i < CONFIGS_PER_PROC * 4; i++)
+		local_match_configs[i] = match_configs[startloc + i];
+
 	for (i = 0; i < 256; i++) {
 		output[i] = CONFIGS_PER_PROC;
 	}
