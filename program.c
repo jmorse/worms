e26b5ffdb33fb9e9d3ffd3fe6829ebc376d52afd
Jeremy Morse <jmorse+git@studentrobotics.org>
2013-05-12 14:50:00 +0100

    Munge OpenCL program further.
    After some eyeballing, seeing how stream processing is /massively SIMD/,
    having the leader/local scenario is massively unoptimal.

--- program.c
+++ program.c
@@ -1,28 +1,12 @@
-void group_leader(__global char *match_configs, __global char *output)
+__kernel void start_trampoline(__global char *match_configs,
+				__global char *output)
 {
+
 	__private unsigned int i;
 	for (i = 0; i < 256; i++) {
 		output[i] = i;
 	}
 	write_mem_fence(CLK_GLOBAL_MEM_FENCE);
 	return;
-}
 
-void group_worker(__global char *match_configs)
-{
-	return;
-}
-
-__kernel void start_trampoline(__global char *match_configs,
-				__global char *output)
-{
-	__local char rts[760];
-	__local char cts[760];
-
-	if (get_local_id(0) == 0)
-		group_leader(match_configs, output);
-	else
-		group_worker(match_configs);
-
-	return;
 }
